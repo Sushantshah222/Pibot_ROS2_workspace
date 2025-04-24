7309999bda0394c2641d3b7b292b11992fd6a901
sushant shah <sushant.shah222@gmail.com>
2025-04-24 07:25:24 +0000

    creating urdf process

---
+++ build/pibot_description/CMakeFiles/pibot_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pibot_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pibot_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
